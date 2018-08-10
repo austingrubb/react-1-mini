69ddeb3086047b8bdb2d2266ad401e79e9157d22
austin <austin.grubb.1995@gmail.com>
2018-08-10 14:51:18 -0700

    delete aded

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: [{
+        name: "austin",
+        picture: "http://http.cat/404"
+      },{
+        name: "dave",
+        picture: "http://http.cat/420"
+      }],
+      name: '',
+      picture: '',
+    }
+  }
+
+  addFriend(){
+    const friend = {
+      name: this.state.name,
+      picture: this.state.picture
+    }
+    const newFriends = this.state.friends.slice()
+    newFriends.push(friend)
+    this.setState({
+      friends: newFriends
+    })
+  }
+
+  deleteFriend(friend){
+   const deleteMe = this.state.friends.findIndex( e => e.name == friend) 
+   console.log(deleteMe)
+    console.log("deleteFriend is working")
+    this.state.friends.splice(deleteMe, 1)
+    this.setState({
+      friends : this.state.friends
+    })
+  }
+  
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  updatePicture(event) {
+    this.setState({
+      picture: event.target.value
+    })
+  }
+
   render() {
+    console.log(this.state.name)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>
+          Name: <input value={this.state.name} onChange={event => this.updateName(event)} />
+          Picture: <input value={this.state.picture} onChange={event => this.updatePicture(event)} />
+          <button onClick={() => this.addFriend()}>Add friend</button>
+          <button onClick={() => this.deleteFriend(this.state.name)}>delete friend</button>
+        </div>
+        {this.state.friends.map((friend, index) => {
+          return <div key ={index}>
+            Name:{friend.name} Picture:<img className="pic" src = {friend.picture}/>
+          </div>
+        })}
       </div>
     );
   }
